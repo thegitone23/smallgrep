1155e201fd6545773d92c2e5c04f0bf92b3e22d0
Mayank <mayankgautam98@gmail.com>
2023-07-12 21:30:00 +0530

    using eprintln! to print errors

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
 	let args: Vec<String> = env::args().collect();
 
 	let config = Config::build(&args).unwrap_or_else(|err| {
-		println!("Problem parsing areguments : {err}");
+		eprintln!("Problem parsing areguments : {err}");
 		process::exit(1);
 	});
 
 	if let Err(e) = smallgrep::run(config) {
-		println!("Application error : {e}");
+		eprintln!("Application error : {e}");
 		process::exit(1);
 	}
 }
